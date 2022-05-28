38c8801eeb284db0caac0c7725ee4b085d08dbe3
Viachaslau Kalevich <cto@noirix.com>
2022-05-28 11:22:18 +0300

    Concurrency Executor Test

--- src/com/noirix/concurrency/ExecutorServiceTest.java
+++ src/com/noirix/concurrency/ExecutorServiceTest.java
@@ -1,9 +1,11 @@
 package com.noirix.concurrency;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 public class ExecutorServiceTest {
@@ -62,13 +64,6 @@ public class ExecutorServiceTest {
             });
         }
 
-        executor.shutdown();
-
-        // Callable, return a future, submit and run the task async
-//        Future<Integer> futureTask1 = executor.submit(() -> {
-//            System.out.println("I'm Callable task.");
-//            return 1 + 1;
-//        });
 
         /* Before Java 8
 
@@ -81,31 +76,53 @@ public class ExecutorServiceTest {
             }
         });*/
 
-//        try {
-//
-//            otherTask("Before Future Result");
-//
-//            // block until future returned a result,
-//            // timeout if the future takes more than 5 seconds to return the result
-//            Integer result = futureTask1.get(5, TimeUnit.SECONDS);
-//
-//            System.out.println("Get future result : " + result);
-//
-//            otherTask("After Future Result");
-//
-//
-//        } catch (InterruptedException e) {// thread was interrupted
-//            e.printStackTrace();
-//        } catch (ExecutionException e) {// thread threw an exception
-//            e.printStackTrace();
-//        } catch (TimeoutException e) {// timeout before the future task is complete
-//            e.printStackTrace();
-//        } finally {
-//
-//            // shut down the executor manually
-//            executor.shutdown();
-//
-//        }
+        // Callable, return a future, submit and run the task async
+//        Future<Integer> futureTask1 = executor.submit(() -> {
+//            System.out.println("I'm Callable task.");
+//            return 1 + 1;
+//        });
+
+        Future<Integer> futureTask1 = executor.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() {
+                System.out.println("I'm Callable task.");
+                //some logic
+                try {
+                    Thread.sleep(3100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                return 100 + 100;
+            }
+        });
+
+        try {
+
+            otherTask("Before Future Result");
+
+            // block until future returned a result,
+            // timeout if the future takes more than 5 seconds to return the result
+            Integer result = futureTask1.get(3, TimeUnit.SECONDS);
+
+            System.out.println("Get future result : " + result);
+
+            otherTask("After Future Result");
+
+
+        } catch (InterruptedException e) {// thread was interrupted
+            e.printStackTrace();
+        } catch (ExecutionException e) {// thread threw an exception
+            e.printStackTrace();
+            System.err.println("Execution error");
+        } catch (TimeoutException e) {// timeout before the future task is complete
+            e.printStackTrace();
+            System.err.println("time out");
+        } finally {
+
+            // shut down the executor manually
+            executor.shutdown();
+
+        }
 
     }
 
